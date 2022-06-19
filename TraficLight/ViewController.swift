1954a630ea1cc9fbf29a61c88f922c0909f6a5ea
DenisKurlykov <dkurlykov@gmail.com>
2022-06-19 14:59:14 +0400

    Traffic light logic

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -7,22 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight {
+    case red, yellow, green
+}
 
+class ViewController: UIViewController {
+    
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
     
     @IBOutlet var startButton: UIButton!
     
+    private var currentLight = CurrentLight.red
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func startButtonTapped() {
+        
+        redLight.layer.cornerRadius = 50
+        redLight.alpha = 0.3
+        yellowLight.layer.cornerRadius = 50
+        yellowLight.alpha = 0.3
+        greenLight.layer.cornerRadius = 50
+        greenLight.alpha = 0.3
+        
+        startButton.layer.cornerRadius = 15
+        
     }
     
+    @IBAction func startButtonTapped() {
+        
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+            
+        case .red:
+            redLight.alpha = 1
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = 0.3
+            yellowLight.alpha  = 1
+            greenLight.alpha  = 0.3
+            currentLight = .green
+        case .green:
+            redLight.alpha  = 0.3
+            yellowLight.alpha  = 0.3
+            greenLight.alpha  = 1
+            currentLight = .red
+        }
+        
+        
+    }
 }
 
